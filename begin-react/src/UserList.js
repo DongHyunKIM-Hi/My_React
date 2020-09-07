ab91969a3c643bd3fae572c044d3fadb2cbba158
DongHyunKIM-Hi <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2020-09-07 19:25:04 +0900

    커스텀 hooks와 context API 연습

--- begin-react/src/UserList.js
+++ begin-react/src/UserList.js
@@ -1,41 +1,40 @@
-import React, { useEffect } from 'react';
-function User({ user,onRemove }) {
-    const {username,email,id,active,onToggle} = user;
-    useEffect(()=>{
-        console.log('user값 후')
-        console.log(user);
-        return()=>{
-            console.log('user값 전');
-            console.log(user)
-        } 
-    }, [user]);
-    return(
+import React from 'react';
+
+const User = React.memo(function User({ user, onRemove, onToggle }) {
+  return (
     <div>
-        <b 
-            style={{
-            color: active ? 'green' : 'black',
-            cursor:"pointer"
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
         }}
-        onClick={()=> onToggle(id)}
-        >{username}</b><span>({email})</span>
-        <button onClick={()=> onRemove(id)}>삭제</button>
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
-    )}
-function UserList({users, onRemove}) {
-    
-    return (
-        <div>
-            {
-           users.map(
-               (user,index)=>(
-                   <User 
-                   user={user} 
-                   key={user.id} 
-                   onRemove={onRemove}/>
-               )
-           )}
-        </div>
-    )
+  );
+});
+
+function UserList({ users, onRemove, onToggle }) {
+  return (
+    <div>
+      {users.map(user => (
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default UserList;
+export default React.memo(
+  UserList,
+  (prevProps, nextProps) => prevProps.users === nextProps.users
+);
