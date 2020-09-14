afd4bd57c437dad191466055a8df32bf7ecc19ad
DongHyunKIM-Hi <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2020-09-14 16:21:40 +0900

    scss를 통한 커스튬

---
+++ sass-pratice/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from "./components/Button";
+import "./App.scss";
+function App() {
+  return (
+    <div className="App">
+      <div className="button">
+        <Button>Button</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
