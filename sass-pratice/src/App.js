12d977c73e8bdc9f910c50c502c1e041f3376f4e
DongHyunKIM-Hi <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2020-09-15 15:44:57 +0900

    sass 연습하기 각 컴포넌트를 classNames의 props로 관리하기

--- sass-pratice/src/App.js
+++ sass-pratice/src/App.js
@@ -4,8 +4,28 @@ import "./App.scss";
 function App() {
   return (
     <div className="App">
-      <div className="button">
+      <div className="buttons">
+        <Button size="large">Button</Button>
         <Button>Button</Button>
+        <Button size="small">Button</Button>
+      </div>
+      <div className="buttons">
+        <Button color="gray" size="large">
+          Button
+        </Button>
+        <Button color="gray">Button</Button>
+        <Button size="small" color="gray">
+          Button
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="pink">
+          Button
+        </Button>
+        <Button color="pink">Button</Button>
+        <Button size="small" color="pink">
+          Button
+        </Button>
       </div>
     </div>
   );
--- sass-pratice/src/components/Button.js
+++ sass-pratice/src/components/Button.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Button.scss";
-
-function Button({ children }) {
-  return <button className="Button">{children}</button>;
+import classNames from "classnames";
+function Button({ children, size, color }) {
+  return (
+    <button className={classNames("Button", size, color)}>{children}</button>
+  );
 }
 
+Button.defaultProps = {
+  size: "medium",
+  color: "blue",
+};
 export default Button;
