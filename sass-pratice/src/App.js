eca93d48a919ccbd1a49ef82548bb8c028e56765
DongHyunKIM-Hi <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2020-09-19 13:50:10 +0900

    style components 연습

--- sass-pratice/src/App.js
+++ sass-pratice/src/App.js
@@ -1,26 +1,80 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import Button from "./components/Button";
 
-const Circle = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background: ${(props) => props.color};
-  border-radius: 50%;
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
 
-  ${(props) =>
-    props.huge &&
-    css`
-      width: 10rem;
-      height: 10rem;
-    `}
+const ButtonGroup = styled.div`
+  & + & {
+    margin-top: 1rem;
+  }
 `;
 
 function App() {
   return (
-    <>
-      <Circle color="blue" huge />
-      <Circle color="black" />;
-    </>
+    <ThemeProvider
+      theme={{
+        palette: {
+          blue: "#228be6",
+          gray: "#495057",
+          pink: "#f06595",
+        },
+      }}
+    >
+      <AppBlock>
+        <ButtonGroup>
+          <Button size="large">BUTTON</Button>
+          <Button>BUTTON</Button>
+          <Button size="small">BUTTON</Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button color="gray" size="large">
+            BUTTON
+          </Button>
+          <Button color="gray">BUTTON</Button>
+          <Button color="gray" size="small">
+            BUTTON
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button color="pink" size="large">
+            BUTTON
+          </Button>
+          <Button color="pink">BUTTON</Button>
+          <Button color="pink" size="small">
+            BUTTON
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size="large" outline>
+            BUTTON
+          </Button>
+          <Button color="gray" outline>
+            BUTTON
+          </Button>
+          <Button color="pink" size="small" outline>
+            BUTTON
+          </Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size="large" fullWidth>
+            BUTTON
+          </Button>
+          <Button size="large" color="gray" fullWidth>
+            BUTTON
+          </Button>
+          <Button size="large" color="pink" fullWidth>
+            삭제하기
+          </Button>
+        </ButtonGroup>
+      </AppBlock>
+    </ThemeProvider>
   );
 }
 
