636a7e828e97884a49b4e56f80f3b5373e408fbe
DongHyunKIM-Hi <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2020-09-19 10:45:38 +0900

    styled-components 연습

--- sass-pratice/src/App.js
+++ sass-pratice/src/App.js
@@ -1,49 +1,26 @@
 import React from "react";
-import Button from "./components/Button";
-import "./App.scss";
+import styled, { css } from "styled-components";
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${(props) => props.color};
+  border-radius: 50%;
+
+  ${(props) =>
+    props.huge &&
+    css`
+      width: 10rem;
+      height: 10rem;
+    `}
+`;
+
 function App() {
   return (
-    <div className="App">
-      <div className="buttons">
-        <Button size="large">Button</Button>
-        <Button>Button</Button>
-        <Button size="small">Button</Button>
-      </div>
-      <div className="buttons">
-        <Button color="gray" size="large">
-          Button
-        </Button>
-        <Button color="gray">Button</Button>
-        <Button size="small" color="gray">
-          Button
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" color="pink">
-          Button
-        </Button>
-        <Button color="pink">Button</Button>
-        <Button size="small" color="pink">
-          Button
-        </Button>
-      </div>
-      <div className="buttons">
-        <Button size="large" outline={true}>
-          Button
-        </Button>
-        <Button color="gray" outline={true}>
-          Button
-        </Button>
-        <Button
-          size="small"
-          color="pink"
-          fullWidth
-          onClick={() => console.log("클릭")}
-        >
-          Button
-        </Button>
-      </div>
-    </div>
+    <>
+      <Circle color="blue" huge />
+      <Circle color="black" />;
+    </>
   );
 }
 
--- useful-tools/src/Sample.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Sample() {
-  return <div>asfdsfdf</div>;
-}
-
-export default Sample;
