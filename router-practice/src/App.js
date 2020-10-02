6b53b2957b02e3d7c8bf2f395fec14b5e1cea4c7
DongHyunKIM-Hi <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2020-10-02 16:11:17 +0900

    router 연습 2

--- router-practice/src/App.js
+++ router-practice/src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import { Route, Link } from "react-router-dom";
 import About from "./About";
 import "./App.css";
+import HistorySample from "./HistorySample";
 import Home from "./Home";
 import Profile from "./Profile";
+import Profiles from "./Profiles";
 
 function App() {
   return (
@@ -15,10 +17,17 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
+        <li>
+          <Link to="/history">예제</Link>
+        </li>
       </ul>
       <Route path="/" component={Home} exact />
       <Route path="/about" component={About} />
-      <Route path="/profiles/:username" component={Profile} />
+      <Route path="/profiles" component={Profiles} />
+      <Route path="/history" component={HistorySample} />
     </div>
   );
 }
