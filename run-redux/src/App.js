2316af96f9a287883d6f4def05b29f6677ddea99
DongHyunKIM-Hi <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2020-10-04 13:28:35 +0900

    redux를 사용하여 투두리스트 민들기

--- run-redux/src/App.js
+++ run-redux/src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./App.css";
 import CounterContaioner from "./containers/CounterContaioner";
+import TodosContainer from "./containers/TodosContainer";
 
 function App() {
-  return <CounterContaioner />;
+  return (
+    <div>
+      <CounterContaioner />
+      <hr></hr>
+      <TodosContainer />
+    </div>
+  );
 }
 
 export default App;
