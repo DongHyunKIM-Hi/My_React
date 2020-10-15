3f0fee252600d2accaa265f8e8bd00628117bf9e
happybird00@naver.com <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2020-10-16 00:24:05 +0900

    ts 컴파일러 설정

---
+++ ts_prac/dist/prac.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ultil_1 = require("./ultil");
+var value = ultil_1.default(1, 2);
+console.log(value);
---
+++ ts_prac/src/prac.ts
@@ -0,0 +1,4 @@
+import add from './ultil';
+
+const value = add(1,2);
+console.log(value);
