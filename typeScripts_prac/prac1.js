07f1e19f8910d1c1348e8ec334b3aa01b40a8121
happybird00@naver.com <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2020-10-15 23:51:44 +0900

    target 옵션 적용해보기

--- typeScripts_prac/prac1.js
+++ typeScripts_prac/prac1.js
@@ -1,2 +1,11 @@
 var test = "test";
-var test1 = 'test1';
+let test1 = 'test1';
+let timeoutPromise = new Promise((resolve, reject) => {
+    setTimeout(("1 sec"));
+});
+{
+    resolve('1 sec');
+}
+1000;
+;
+timeoutPromise.then(console.log);
--- typeScripts_prac/prac1.ts
+++ typeScripts_prac/prac1.ts
@@ -1,3 +1,10 @@
 var test = "test";
 let test1 = 'test1';
 
+let timeoutPromise = new Promise((resolve, reject)=>{
+    setTimeout(("1 sec")=>{
+        resolve('1 sec');
+    },1000);
+})
+
+timeoutPromise.then(console.log);
