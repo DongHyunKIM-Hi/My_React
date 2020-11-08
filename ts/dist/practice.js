5e26ce9a583726f2eeab989b554874bc9cd41c2d
happybird00@naver.com <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2020-11-08 14:49:17 +0900

    interface 연습1

---
+++ ts/dist/practice.js
@@ -0,0 +1,24 @@
+"use strict";
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    getArea() {
+        return this.radius * this.radius * Math.PI;
+    }
+}
+class React {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    getArea() {
+        return this.width * this.height;
+    }
+}
+const circle = new Circle(5);
+const react = new React(4, 5);
+const shapes = [circle, react];
+shapes.forEach(me => {
+    console.log(me.getArea());
+});
--- ts/src/practice.ts
+++ ts/src/practice.ts
@@ -1,2 +1,34 @@
-const message: string = 'hello world';
-console.log(message)
+interface Shape{
+    getArea(): number;
+}
+
+class Circle implements Shape{
+    radius: number;
+
+    constructor(radius: number){
+        this.radius = radius;
+    }
+    getArea(){
+        return this.radius * this.radius* Math.PI;
+    }
+}
+
+class React implements Shape{
+    width: number;
+    height: number;
+    constructor(width: number, height: number){
+        this.width = width;
+        this.height = height;
+    }
+    getArea(){
+        return this.width * this.height;
+    }
+}
+
+const circle = new Circle(5);
+const react = new React(4,5);
+const shapes: Shape[] = [circle, react];
+
+shapes.forEach(me=>{
+    console.log(me.getArea());
+})
