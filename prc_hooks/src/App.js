2ed17ca13d64f8c4aed66a2c507b34b65861ab91
Viva <happybird00@naver.com>
2020-12-22 22:13:34 +0900

    useReducer 다시한번 연습해보기

--- prc_hooks/src/App.js
+++ prc_hooks/src/App.js
@@ -4,117 +4,66 @@ import Create from "./test1/Create";
 import UserList from "./test1/UserList";
 
 export default function App() {
-  const nextId = useRef(4);
+  function reducer(state, action) {
+    switch (action.type) {
+      case "change":
+        return {
+          input: {
+            ...state.input,
+            [action.name]: action.value,
+          },
+        };
+      case "create":
+        return {
+          input: {
+            ...state.input,
+          },
+          users: state.users.concat(state.input),
+        };
+      default:
+        throw new Error("뭐가 틀린겨");
+    }
+  }
   const initialState = {
-    user: {
-      username: "",
+    input: {
+      name: "",
       age: "",
     },
     users: [
       {
         id: 1,
-        username: "Viva",
-        age: "24",
-        active: true,
+        name: "Viva",
+        age: "25",
       },
       {
         id: 2,
-        username: "Vida",
-        age: "24",
-        active: true,
-      },
-      {
-        id: 3,
-        username: "shu",
-        age: "24",
-        active: true,
+        name: "la",
+        age: "20",
       },
     ],
   };
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const { username, age } = state.user;
-  const { users } = state;
-  function reducer(state, action) {
-    switch (action.type) {
-      case "change":
-        return {
-          ...state,
-          user: {
-            ...state.user,
-            [action.name]: action.value,
-          },
-        };
-      case "inroll":
-        return {
-          user: {
-            username: "",
-            age: "",
-          },
-          users: state.users.concat(action.user),
-        };
-      case "remove":
-        return {
-          ...state,
-          users: state.users.filter((user) => user.id !== action.id),
-        };
-      case "toggle":
-        return {
-          ...state,
-          users: state.users.map((user) =>
-            user.id === action.id ? { ...user, active: !user.active } : user
-          ),
-        };
-      default:
-        throw new Error("시발 뭐가 틀린겨");
-    }
-  }
 
-  const onChagne = (e) => {
-    const { name, value } = e.target;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { name, age } = state.input;
+  const { users } = state;
+  const onChange = (e) => {
     dispatch({
       type: "change",
-      name,
-      value,
+      name: e.target.name,
+      value: e.target.value,
     });
+    console.log(state.input);
   };
-  const inRoll = () => {
+  const onCreate = () => {
     dispatch({
-      type: "inroll",
-      user: {
-        id: nextId.current,
-        username,
-        age,
-      },
-    });
-    nextId.current += 1;
-  };
-  const reMove = (id) => {
-    dispatch({
-      type: "remove",
-      id,
+      type: "create",
     });
   };
-
-  const onToggle = (id) => {
-    dispatch({
-      type: "toggle",
-      id,
-    });
-  };
-  const countNum = (users) => {
-    return users.filter((user) => user.active === true).length;
-  };
-  const num = countNum(users);
   return (
     <>
-      <Create
-        username={username}
-        age={age}
-        onChange={onChagne}
-        inRoll={inRoll}
-      />
-      <UserList users={users} reMove={reMove} onToggle={onToggle} />
-      {num}
+      <input placeholder="이름" name="name" value={name} onChange={onChange} />
+      <input placeholder="나이" name="age" value={age} onChange={onChange} />
+      <button onClick={onCreate}>등록하기</button>
     </>
   );
 }
