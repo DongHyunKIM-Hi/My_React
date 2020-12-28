7e3b9801e673e97e20ef8a4998a6353e17fee967
Viva <happybird00@naver.com>
2020-12-28 22:25:19 +0900

    class 형 컴포넌트로 React 수정

--- prc_hooks/src/Counter.js
+++ prc_hooks/src/Counter.js
@@ -1,26 +1,19 @@
-import React, { useReducer } from "react";
+import React, { Component } from "react";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "case1":
-      return state + 1;
-    case "case2":
-      return state - 1;
-    default:
-      throw new Error();
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+  render() {
+    return (
+      <div>
+        {this.props.state.count}
+        <button onClick={() => this.props.counter(count + 1)}>+</button>
+        <button onClick={() => this.props.setState(count - 1)}>-</button>
+      </div>
+    );
   }
 }
-
-function Counter() {
-  const [state, dispatch] = useReducer(reducer, 0);
-
-  return (
-    <>
-      {state}
-      <button onClick={() => dispatch({ type: "case1" })}>+</button>
-      <button onClick={() => dispatch({ type: "case2" })}>-</button>
-    </>
-  );
-}
-
-export default Counter;
