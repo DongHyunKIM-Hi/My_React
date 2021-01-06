fcb0d5efe9ae4bb2b1bfbc23738216814d16de85
Viva <happybird00@naver.com>
2021-01-06 16:04:31 +0900

    준비준비

---
+++ deme1/src/Manage/Context.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Context() {
+    const makeList = [
+        {
+            id:1,
+            name: '투두 리스트 만들어보기',
+            active: false,
+        },
+        {
+            id:2,
+            name: '알고리즘 문제 풀어보기',
+            active: false,
+        },
+        {
+            id:3,
+            name: '대학 강의 듣기',
+            active: false,
+        }
+    ]
+    const []
+    return;
+}
+
+export default Context;
